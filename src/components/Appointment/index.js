a6576334a5c4aea08eb1673f098b78e6ffcf2703
Lucas Mallmann <lucasmallmann76@gmail.com>
2019-11-15 11:12:18 -0300

    Appointment componentn

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,59 @@
-import React from 'react';
-// import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import PropTypes from 'prop-types';
 
-import { Container } from './styles';
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointments() {
-  return <Container />;
+export default function Appointments({ data, onCancel }) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]);
+
+  return (
+    <Container past={data.past}>
+      <Left>
+        <Avatar
+          source={{
+            uri: data.provider.avatar
+              ? `http://10.0.3.2:3333/files/${data.provider.avatar.path}`
+              : `https://api.adorable.io/avatar/50/${data.provider.id}`,
+          }}
+        />
+
+        <Info>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
+        </Info>
+      </Left>
+
+      {data.cancelable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
+    </Container>
+  );
 }
+
+Appointments.propTypes = {
+  data: PropTypes.shape({
+    date: PropTypes.string,
+    past: PropTypes.bool,
+    provider: PropTypes.shape({
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+        path: PropTypes.string,
+      }),
+      name: PropTypes.string,
+    }),
+    cancelable: PropTypes.bool,
+    canceled_at: PropTypes.string,
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
